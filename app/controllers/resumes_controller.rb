912ada08a83e413424802f6ee8e355657f195aec
Eddie Kao <eddie@5xcampus.com>
2023-03-23 15:00:54 +0800

    WIP - create resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,13 @@
 class ResumesController < ApplicationController
+
   def index
   end
 
   def new
   end
 
+  def create
+    # 寫入資料庫
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/resumes', to: 'resumes#index'
   get '/resumes/new', to: 'resumes#new', as: 'new_resume'
+  post '/resumes', to: 'resumes#create'
 
   root 'pages#home'
   # get '/', to: 'pages#home', as: 'root'
