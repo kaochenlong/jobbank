e96d96764d380050cdc1b34b081a77b64d51935a
Eddie Kao <eddie@5xcampus.com>
2023-03-23 17:06:26 +0800

    WIP - create resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,13 +1,24 @@
 class ResumesController < ApplicationController
-
   def index
   end
 
   def new
+    @resume = Resume.new
   end
 
   def create
-    # 寫入資料庫
-    redirect_to root_path
+    resume = Resume.new(resume_params)
+
+    if resume.save
+      redirect_to root_path
+    else
+      render html: params
+    end
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:name, :email, :tel, :skill, :intro, :city, :education, :experience, :portfolio)
   end
 end
