e60f6cf832d65da9a6fbf796d82b7e474b19ff09
Eddie Kao <eddie@5xcampus.com>
2023-03-23 12:46:27 +0800

    add navbar

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,8 @@
+class ResumesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/resumes', to: 'resumes#index'
+  get '/resumes/new', to: 'resumes#new', as: 'new_resume'
+
+  root 'pages#home'
+  # get '/', to: 'pages#home', as: 'root'
+
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
 end
