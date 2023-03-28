085c931343e2caaf5cd5e901103951946145412f
Eddie Kao <eddie@5xcampus.com>
2023-03-28 16:46:13 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  delete '/resumes/:id', to: 'resumes#destroy'
-  patch '/resumes/:id', to: 'resumes#update'
-  get '/resumes/:id/edit', to: 'resumes#edit', as: 'edit_resume'
-  get '/resumes/new', to: 'resumes#new', as: 'new_resume'
-  get '/resumes/:id', to: 'resumes#show', as: 'resume'
-  get '/resumes', to: 'resumes#index'
-  post '/resumes', to: 'resumes#create'
+  # delete '/resumes/:id', to: 'resumes#destroy'
+  # patch '/resumes/:id', to: 'resumes#update'
+  # put '/resumes/:id', to: 'resumes#update'
+  # get '/resumes/:id/edit', to: 'resumes#edit', as: 'edit_resume'
+  # get '/resumes/new', to: 'resumes#new', as: 'new_resume'
+  # get '/resumes/:id', to: 'resumes#show', as: 'resume'
+  # get '/resumes', to: 'resumes#index'
+  # post '/resumes', to: 'resumes#create'
+  resources :resumes
 
   root 'pages#home'
-  # get '/', to: 'pages#home', as: 'root'
 
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
