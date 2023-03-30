f3be0339101fa8e7156df0ba3e5bf4a1769d502c
Eddie Kao <eddie@5xcampus.com>
2023-03-30 11:19:13 +0800

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  # delete '/resumes/:id', to: 'resumes#destroy'
-  # patch '/resumes/:id', to: 'resumes#update'
-  # put '/resumes/:id', to: 'resumes#update'
-  # get '/resumes/:id/edit', to: 'resumes#edit', as: 'edit_resume'
-  # get '/resumes/new', to: 'resumes#new', as: 'new_resume'
-  # get '/resumes/:id', to: 'resumes#show', as: 'resume'
-  # get '/resumes', to: 'resumes#index'
-  # post '/resumes', to: 'resumes#create'
+  root "pages#home"
+
+  resource :user, except: %i[new destroy] do
+    get "sign_up", action: "new"
+  end
+
   resources :resumes
 
-  root 'pages#home'
-
-  get '/about', to: 'pages#about'
-  get '/contact', to: 'pages#contact'
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
 end
