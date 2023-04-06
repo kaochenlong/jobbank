258333a7a672d6ae48e264cba2e1de92d8bb4886
Eddie Kao <eddie@5xcampus.com>
2023-04-06 17:00:19 +0800

    use stimulus.js

---
+++ app/javascript/controllers/comment_form_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["button"]
+
+  input(e) {
+    const content = e.target.value.trim()
+
+    if (content != "") {
+      // 開
+      this.buttonTarget.classList.remove("btn-disable")
+      this.buttonTarget.classList.add("btn-primary")
+      this.buttonTarget.removeAttribute("disabled")
+    } else {
+      // 關
+      this.buttonTarget.classList.remove("btn-primary")
+      this.buttonTarget.classList.add("btn-disable")
+      this.buttonTarget.setAttribute("disabled", "")
+    }
+  }
+}
